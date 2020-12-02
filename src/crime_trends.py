63cadc02a4a7b4fbc01bbc6ab41c1284bd5c9e2f
Ece Varol <ecevarol8@gmail.com>
2020-12-01 23:56:53 -0600

    crime analysis

--- src/crime_trends.py
+++ src/crime_trends.py
@@ -4,6 +4,11 @@ import matplotlib as plt
 df = pd.read_csv("../clean_data.csv")
 print("Finished reading csv")
 
+#Remove 2001
+list = df[df['Year'] == 2001].index.tolist()
+df.drop(list, inplace = False)
+
+#Graph the general trend
 groups = df.groupby(["Year", "Month"]).size()
 ax = groups.plot()
 fig = ax.get_figure()
