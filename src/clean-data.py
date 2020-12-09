1af22816bb702838876f0d83ef52d280dac97e3e
Ece Varol <ecevarol8@gmail.com>
2020-12-09 15:52:49 -0600

    clean data.py updated

--- src/clean-data.py
+++ src/clean-data.py
@@ -4,8 +4,13 @@ import pandas as pd
 # download from: https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-Present/ijzp-q8t2/data?fbclid=IwAR0bFz89WTpotKU6dbm6VIudWC3kb5jm0s8uNKGMrLXnkq-0tLO68pmg40c
 
 
-df = pd.read_csv("../data/Crimes_-_2001_to_Present.csv", na_values = " ")
-#delete rows that have a missing Community Area value
+df = pd.read_csv("../data/Crimes_-_2001_to_Present.csv", na_values = " ", low_memory=False)
+
+#Remove data before 2010
+list = df[df['Year'] < 2010].index.tolist()
+df.drop(list, inplace = False)
+
+#Delete rows that have a missing Community Area value
 list = df[df['Community Area'].isnull()].index.tolist()
 df.drop(list,inplace = True)
 df.index = range(len(df))
@@ -20,4 +25,24 @@ df["Day"] = date_spec[1]
 df["Time"] = date[1]
 df["Date"] = date[0]
 
-df.to_csv("../data/clean_data.csv")
+#add a crime label column using https://www.justia.com/criminal/offenses/ categories
+property_crimes = "BURGLARY|THEFT|LARCENY|ROBBERY|SHOPLIFTING|PROPERTY|DAMAGE"
+personal_crimes = "ASSAULT|BATTERY|ARSON|ABUSE|KIDNAPPING|RAPE|SEX|CHILDREN|HOMICIDE|MURDER"
+financial_crimes = "FRAUD|DECEPTIVE PRACTICE|TAX|BLACKMAIL"
+statutory_crimes = "NARCOTIC|ALCOHOL|DRUG|WEAPONS"
+
+df["type"] = "Other"
+
+mask = df['Primary Type'].str.contains(property_crimes)
+df.loc[mask, 'type'] = "PROPERTY"
+
+mask = df['Primary Type'].str.contains(personal_crimes)
+df.loc[mask, 'type'] = "PERSONAL"
+
+mask = df['Primary Type'].str.contains(financial_crimes)
+df.loc[mask, 'type'] = "FINANCIAL"
+
+mask = df['Primary Type'].str.contains(statutory_crimes)
+df.loc[mask, 'type'] = "STATUTORY"
+
+df.to_csv("../clean_data.csv")
